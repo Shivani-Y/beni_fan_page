c72028056594f499769215e5bbc4e8b7cd53036a
Shivani Yadav <yadashiv@sheridancollege.ca>
2020-02-18 07:56:48 +0530

    added  comment model

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,46 @@ from django.contrib import admin
 from . import models
 
 # Register the `Post` model
-admin.site.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    """customising post model view"""
+    list_display = (
+        'title',
+        'author',
+        'created',
+        'updated',
+    )
+    #defining how to search on the page
+    search_fields = (
+        'title',
+        'author__username',
+        'author__first_name',
+        'author__last_name',
+    )
+    #we can now filter on the basis of status all, draft or published
+    list_filter = (
+        'status',
+    )
+    prepopulated_fields = {'slug': ('title',)}
+
+class CommnetAdmin(admin.ModelAdmin):
+    """customising post model view"""
+    list_display = (
+        'name',
+        'text',
+        'approved',
+        'created',
+        'updated',
+    )
+    #defining how to search on the page
+    search_fields = (
+        'text',
+    )
+    #we can now filter on the basis of
+    list_filter = (
+        'status',
+    )
+
+# Register the `Post` model
+admin.site.register(models.Post, PostAdmin)
+# Register the `Comment` model
+admin.site.register(models.Comment, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 """Creating models for the app"""
+from django.conf import settings  # Imports Django's loaded settings
 from django.db import models
 
 # Create your models here.
@@ -6,7 +7,60 @@ class Post(models.Model):
     """
     Represents a blog post
     """
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published')
+    ]
     title = models.CharField(max_length=255)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
     updated = models.DateTimeField(auto_now=True)  # Updates on each savenote
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # The Django auth user model
+        on_delete=models.PROTECT,  # Prevent posts from being deleted
+        related_name='blog_posts',  # "This" on the user model
+        null=False
+    )
+    published = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text='The date & time this article was published',
+    )
+    slug = models.SlugField(
+        null=False,
+        help_text='The date & time this article was published',
+        unique_for_date='published',  # Slug is unique for publication date
+    )
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=DRAFT,
+        help_text='Set to "published" to make this post publicly visible',
+    )
+
+    class Meta:
+        """sorting the posts in reverse"""
+        # Sort by the `created` field. The `-` prefix
+        # specifies to order in descending/reverse order.
+        # Otherwise, it will be in ascending order.
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    """
+    Represents a comments section
+    """
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, required=True)
+    email = models.EmailField(max_length=254, required=True)
+    text = models.CharField(max_length=255, required=True)
+    approved = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)  # Sets on create
+    updated = models.DateTimeField(auto_now=True)  # Updates on each save
+
+    def __str__(self):
+        return self.name
